d11038669b7a53a3dd6d21149a8a936de8df7304
ujez <ujezujez@gmail.com>
2022-07-08 14:12:18 +0100

    little update

--- main.tf
+++ main.tf
@@ -20,5 +20,6 @@ module "ec2_cluster" {
 
 
 output "public_ip" {
-  value = ec2_cluster.name.public_ip
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = try(aws_instance.this[0].public_ip, aws_spot_instance_request.this[0].public_ip, "")
 }
