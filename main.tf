8e1fedf7ca3a00e65789b5cde91ba0adcd7c7f69
ujez <ujezujez@gmail.com>
2022-07-08 13:28:40 +0100

    little update

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 module "ec2_cluster" {
-    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-    name    = "my-cluster"
-    ami     = "ami-0f65ab0fd913bc7be"
-    instance_type          = "t2.micro"
-    subnet_id   = "subnet-0b82885deb904d684"
+  name          = "my-cluster"
+  ami           = "ami-0f65ab0fd913bc7be"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0b82885deb904d684"
+  key_name      = "levelup_key4"
+  public_key    = file(var.PATH_TO_PUBLIC_KEY)
 
-    tags = {
+  tags = {
     Terraform   = "true"
     Environment = "dev"
-    }
+  }
 }
