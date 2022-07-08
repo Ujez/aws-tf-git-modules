ec5cc7fd415016dbb5c747854b0525aab5cf881c
ujez <ujezujez@gmail.com>
2022-07-08 13:46:17 +0100

    little update

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "levelup_key" {
+  key_name   = "levelup_key4"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
 module "ec2_cluster" {
   source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
@@ -5,8 +10,7 @@ module "ec2_cluster" {
   ami           = "ami-0f65ab0fd913bc7be"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0b82885deb904d684"
-  key_name      = "levelup_key4"
-  public_key    = file(var.PATH_TO_PUBLIC_KEY)
+  key_name      = aws_key_pair.levelup_key.key_name
 
   tags = {
     Terraform   = "true"
